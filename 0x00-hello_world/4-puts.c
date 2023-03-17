9a5250c7a809d3cc48a8c24d140f24667d68365a
Londeka-N <londeka.immaculaten@gmail.com>
2023-03-17 09:31:16 -0700

    taske 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+include <stdio.h>
+
+/** main - every code must be inclsed by main function
+ *return : must alwaays be 0 for successful output
+ */
+
+int main (void)
+{
+	puts("\"Programmingis like building a multilingual puzzele"\n)
+	return (0)
+}
