50d71d78d0b521b4395bd30857bdac1df1a6e48e
Londeka-N <londeka.immaculaten@gmail.com>
2023-03-17 21:44:33 -0700

    task 5 done and dustes, hopefully

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+include < stdio.h >
+
+/**
+ * main - "entry point for all my script"
+ * all my code will be enlosed within the main function
+ * Return: always 0 to ensure successful output
+ *
+ */
+
+int main(void)
+
+{
+	printf(with proper grammar, but the outcome is a piece of art, \n)
+	Return(0)
+}
+
