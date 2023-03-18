f510b12d1264069b20c9169c690bd6f92dbd7350
Londeka-N <londeka.immaculaten@gmail.com>
2023-03-17 23:23:40 -0700

    use printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,17 @@
-include < stdio.h >
+#include <stdio.h>
 
 /**
- * main - "entry point for all my script"
+ * main -"entry point for all my script"
  * all my code will be enlosed within the main function
+ *
  * Return: always 0 to ensure successful output
  *
- */
+*/
 
 int main(void)
 
 {
-	printf(with proper grammar, but the outcome is a piece of art, \n)
-	Return(0)
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Return(0);
 }
 
