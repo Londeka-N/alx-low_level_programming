b5c4185ed58dc3b53deba73c00ed42faaca9bfbb
Londeka-N <londeka.immaculaten@gmail.com>
2023-03-19 15:45:54 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,11 @@
  * main -entry point for my script
  * my code will be enclosed by main function
  *
- * Return: 1 always for succeccful output0
+ * Return: 0 always for succeccful output0
 */
 
 int main(void)
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+}
