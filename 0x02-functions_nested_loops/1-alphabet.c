5a2e6d4b0b8b4db759e411b0d8e8b784522dc471
Londeka-N <londeka.immaculaten@gmail.com>
2023-03-24 13:06:52 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - entry point
+ * print_alphabet -  print alphabets
+ *
+ *Return: 0 for now
+*/
+
+void print_alphabet(void);
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+}
