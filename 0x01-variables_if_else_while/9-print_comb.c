870fa0e5758dbad2a5a41f3a3df18868fecfaf3a
Londeka-N <londeka.immaculaten@gmail.com>
2023-03-23 18:23:42 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,12 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		if (i == 9)
-			putchar(i + '0');
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i == 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
