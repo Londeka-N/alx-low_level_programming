037c696a3ff3094c67e07f9694fb2d799a20ec14
Londeka-N <londeka.immaculaten@gmail.com>
2023-03-27 10:32:30 -0700

    pointers0

--- 0-reset_to_98.c/0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0-reset_to_98.c/0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * reset_to_98 - a function that takes a pointer to an int as parameter
+ * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to to 98
- * @n: input
- * Return: n
+ * @n: input to updat
+ * Return: 0 for now
  */
 void reset_to_98(int *n)
 {
