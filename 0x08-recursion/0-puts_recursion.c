aeef7d910f9a7eac2b44170ef93bcd0d4761d098
Londeka-N <londeka.immaculaten@gmail.com>
2023-04-05 12:22:42 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts_recursion - a function used to print a string
+ * @s: name of the characiter
+ *
+ * Return: 0 for now
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+
+}
