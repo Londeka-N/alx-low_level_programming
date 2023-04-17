d851e801c5249c3adae5fbc735bd7fe842114483
Londeka-N <londeka.immaculaten@gmail.com>
2023-04-17 15:29:38 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
 /**
-* struct dog - a dog's basic info
-* @name: Name of the dog
-* @age: age of the dog
-* @owner: the keeper of ther dog
+ * struct dog - a dog's basic info
+ * @name: Name of the dog
+ * @age: age of the dog
+ * @owner: the keeper of ther dog
 */
 struct dog
 {
@@ -18,5 +18,7 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
