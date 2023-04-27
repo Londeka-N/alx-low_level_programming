671cbe2ede00662198ff22b2eb3a26f38f9145f0
Londeka-N <londeka.immaculaten@gmail.com>
2023-04-27 00:22:53 -0700

    task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - a function that locates a character in a string
+ * @s: The string of interes
+ * @c: The character of interest
+ *
+ * Return: 0 for succeful output
+*/
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+}
