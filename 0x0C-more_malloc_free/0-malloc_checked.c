cf7b6a57067582e106d8d4aefb8f85eb27101cfe
Londeka-N <londeka.immaculaten@gmail.com>
2023-05-01 06:05:06 -0700

    a function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+/**
+ * *malloc_checked = a function that will allocate memory
+ * @b: number of interest
+ * Return: 0 for now
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *p
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		(Exit98);
+	}
+
+return (0);
+
