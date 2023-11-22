b33d1d721fb5ffb1bbd8f2eb7613b7611f0cb27b
Londeka-N <londeka.immaculaten@gmail.com>
2023-11-22 05:55:09 -0800

    i hope this works

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_numbers - a function that print out numbers
+ * main - encloses code
+ *
+ * Return: 0 when successful
+*/
+
+int main(void)
+{
+	void print_numbers(void);
+	return (0);
+}
+
+void print_numbers(void)
+{
+	int num = 0
+		while (num < 10)
+		{
+			_putchar(num);
+			num++
+		}
+	return (0);
+}
