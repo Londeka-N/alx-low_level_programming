d5b3747265acffeaf3d6eb0b1b3a9b79a5275901
Londeka-N <londeka.immaculaten@gmail.com>
2023-11-28 01:29:45 -0800

    i hope this works

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,19 +6,29 @@
  * Return: 0 when successful
 */
 
+void print_numbers(void);
+
+/**
+ * main - encloses the code
+ * Return: 0 when successful
+*/
+
 int main(void)
 {
-	void print_numbers(void);
+	print_numbers();
 	return (0);
 }
 
+/**
+ * print_numbers - a function that prints out numbers
+*/
+
 void print_numbers(void)
 {
-	int num = 0
+	int num = 0;
 		while (num < 10)
 		{
-			_putchar(num);
-			num++
+			_putchar('0' + num);
+			num++;
 		}
-	return (0);
 }
