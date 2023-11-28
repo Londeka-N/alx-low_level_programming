50c5f34becaac668963513af1d663eb8061649b0
Londeka-N <londeka.immaculaten@gmail.com>
2023-11-28 05:04:16 -0800

    firsty try

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - function that print 0-14 ten times
+ *
+ * Return: 0  when successful
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int num;
+
+	for (i = 0; i < 10; i++)
+	{
+		num = 0;
+
+		if (num >= 0 && num <= 14)
+		{
+			_putchar('0' + num);
+			num++;
+		}
+
+		_putchar('\n');
+	}
+}
